66ef15a9362b24ff634e3699266963a475516ee5
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-11-22 09:28:15 -0500

    ready to start building

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import userEvent from '@testing-library/user-event';
+import { Routes, Route } from 'react-router-dom';
+import Login from './scenes/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {
+          !userEvent
+          ? <Route index element={<Login />} />
+          : <Route index element={<Login />} />
+        }
+      </Routes>
     </div>
   );
 }
