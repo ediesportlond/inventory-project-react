073ce87ec5c6373e06fc5aaec49be0ba83750d33
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-12-01 14:46:13 -0500

    selector works

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -1,17 +1,56 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { UserContext } from '../App';
 import { Button } from 'antd';
 import '../assets/selector.css';
 
 export default function Selector({ setList }) {
-  const [selected, setSelected] = useState()
+  const { token, setUser, setToken } = useContext(UserContext);
+  const [selected, setSelected] = useState();
+
 
   const getSelected = (selected) => {
     setSelected(selected)
+    fetch(`${process.env.REACT_APP_ENDPOINT}/inventory/select/${selected}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      }
+    })
+      .then(res => {
+        if (res.status === 401) {
+          setUser()
+          setToken()
+          sessionStorage.removeItem('user')
+          sessionStorage.removeItem('token')
+        }
+        return res.json()
+      })
+      .then((result) => setList(result.message))
+      .catch(console.error)
 
   }
 
   const getAll = () => {
     setSelected('all')
+    fetch(process.env.REACT_APP_ENDPOINT + '/inventory', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      }
+    })
+      .then(res => {
+        if (res.status === 401) {
+          setUser()
+          setToken()
+          sessionStorage.removeItem('user')
+          sessionStorage.removeItem('token')
+        }
+        return res.json()
+      })
+      .then((result) => setList(result.message))
+      .catch(console.error)
   }
 
   return (
@@ -25,22 +64,22 @@ export default function Selector({ setList }) {
               <Button danger ghost onClick={() => getSelected('nostock')}>Out of stock</Button>
             </>
             : selected === 'nostock'
-            ? <>
-              <Button type='primary' ghost onClick={() => getSelected('instock')}>In stock</Button>
-              <Button type='dashed' onClick={getAll}>All</Button>
-              <Button danger onClick={() => getSelected('nostock')}>Out of stock</Button>
-            </>
-            : selected === 'all'
-            ? <>
-              <Button type='primary' ghost onClick={() => getSelected('instock')}>In stock</Button>
-              <Button onClick={getAll}>All</Button>
-              <Button danger ghost onClick={() => getSelected('nostock')}>Out of stock</Button>
-            </>
-            : <>
-            <Button type='primary' ghost onClick={() => getSelected('instock')}>In stock</Button>
-            <Button type='dashed' onClick={getAll}>All</Button>
-            <Button danger ghost onClick={() => getSelected('nostock')}>Out of stock</Button>
-          </>
+              ? <>
+                <Button type='primary' ghost onClick={() => getSelected('instock')}>In stock</Button>
+                <Button type='dashed' onClick={getAll}>All</Button>
+                <Button danger onClick={() => getSelected('nostock')}>Out of stock</Button>
+              </>
+              : selected === 'all'
+                ? <>
+                  <Button type='primary' ghost onClick={() => getSelected('instock')}>In stock</Button>
+                  <Button onClick={getAll}>All</Button>
+                  <Button danger ghost onClick={() => getSelected('nostock')}>Out of stock</Button>
+                </>
+                : <>
+                  <Button type='primary' ghost onClick={() => getSelected('instock')}>In stock</Button>
+                  <Button type='dashed' onClick={getAll}>All</Button>
+                  <Button danger ghost onClick={() => getSelected('nostock')}>Out of stock</Button>
+                </>
         }
       </div>
     </>
